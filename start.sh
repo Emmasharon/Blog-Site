0795524b235b40c4826c3a00531689f18635f37a
Emma Sharon <emmasharon19@gmail.com>
2019-07-08 16:44:02 +0300

    add secret key and email password to the start.sh file

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='qwerty12345'
+export DATABASE_URL='postgresql+psycopg2://emma:1234@localhost/bloggy'
+export MAIL_USERNAME='emmasharon19@gmail.com'
+export MAIL_PASSWORD='emmasharon2'
+python3.6 manage.py server
